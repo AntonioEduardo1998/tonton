fa1998723fb954cfc24be6cd6ff6fb7d42acdd78
Antonio Eduardo Ribeiro de Oliveira <antonioeduardo1998@gmail.com>
2024-08-16 15:50:04 -0300

    refactor(home): wrap component with safe-area-view

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,8 +1,10 @@
+import { Header } from '@components/Header';
 import { Container, Title } from '@screens/Home/styles';
 
 export function Home() {
   return (
     <Container>
+      <Header showCartButton />
       <Title>Home</Title>
     </Container>
   );
