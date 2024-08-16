2760f6489fbb5cac35780e6a9350c195f49e87c8
Antonio Eduardo Ribeiro de Oliveira <antonioeduardo1998@gmail.com>
2024-08-16 15:47:00 -0300

    feat(app): setup statusBar element

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import { Routes } from '@routes/index';
 import theme from 'src/theme';
 import { ThemeProvider } from 'styled-components/native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
-import { ActivityIndicator } from 'react-native';
+import { ActivityIndicator, StatusBar } from 'react-native';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -11,6 +11,9 @@ export default function App() {
   });
 
   return (
-    <ThemeProvider theme={theme}>{fontsLoaded ? <Routes /> : <ActivityIndicator />}</ThemeProvider>
+    <ThemeProvider theme={theme}>
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+      {fontsLoaded ? <Routes /> : <ActivityIndicator />}
+    </ThemeProvider>
   );
 }
