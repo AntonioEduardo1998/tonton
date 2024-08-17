bd312755d9e7621b77d244e4f6941b11e2d6d48a
Antonio Eduardo Ribeiro de Oliveira <antonioeduardo1998@gmail.com>
2024-08-17 14:34:54 -0300

    feat(home): add title

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -5,7 +5,9 @@ import { Container } from '@screens/Home/styles';
 export function Home() {
   return (
     <Container>
-      <Header showCartButton />
+      <Header showCartButton>
+        <Header.Title>jogos do dia</Header.Title>
+      </Header>
       <GameList />
     </Container>
   );
--- src/screens/home/styles.ts
+++ src/screens/home/styles.ts
@@ -3,5 +3,5 @@ import styled from 'styled-components/native';
 
 export const Container = styled(SafeAreaView)`
   flex: 1;
-  background-color: ${({ theme }) => theme.COLORS.GRAY_600};
+  background-color: ${(props) => props.theme.COLORS.PURPLE_500};
 `;
