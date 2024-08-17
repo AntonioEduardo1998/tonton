daf85027ece10d0d1bf85f1c5b5ab76fb20aeafa
Antonio Eduardo Ribeiro de Oliveira <antonioeduardo1998@gmail.com>
2024-08-17 18:30:40 -0300

    feat(clear-cart-button): show confirmation dialog before clear cart

--- src/screens/ShoppingCart/components/ClearCartButton/index.tsx
+++ src/screens/ShoppingCart/components/ClearCartButton/index.tsx
@@ -2,18 +2,28 @@ import { Text } from '@components/Text';
 import { MaterialIcons } from '@expo/vector-icons';
 import { Container } from '@screens/ShoppingCart/components/ClearCartButton/styles';
 import { clearCart } from '@store/slices/cart.slice';
+import { showToast } from '@utils/showToast';
+import { Alert } from 'react-native';
 import { useDispatch } from 'react-redux';
 import theme from 'src/theme';
 
 export function ClearCartButton() {
   const dispatch = useDispatch();
 
-  const handleClearCart = () => {
+  function handleClearCart() {
     dispatch(clearCart());
-  };
+    showToast('Carrinho esvaziado');
+  }
+
+  function confirmClearCart() {
+    Alert.alert('Remover todos os itens do carrinho?', '', [
+      { text: 'cancelar', style: 'cancel' },
+      { text: 'remover', onPress: handleClearCart, style: 'destructive' },
+    ]);
+  }
 
   return (
-    <Container onPress={handleClearCart}>
+    <Container onPress={confirmClearCart}>
       <MaterialIcons name="delete" size={theme.FONT_SIZE.LG} color={theme.COLORS.RED} />
       <Text size={theme.FONT_SIZE.SM} fontFamily={theme.FONT_FAMILY.BOLD} color={theme.COLORS.RED}>
         Remover todos
