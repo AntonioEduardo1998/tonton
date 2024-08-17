335d4c2150a4fb7c5d65e76004b736e3a0a57035
Antonio Eduardo Ribeiro de Oliveira <antonioeduardo1998@gmail.com>
2024-08-17 14:35:21 -0300

    feat(shopping-cart): implement shopping cart component

--- src/screens/ShoppingCart/index.tsx
+++ src/screens/ShoppingCart/index.tsx
@@ -1,11 +1,47 @@
 import { Header } from '@components/Header';
-import { Container, Title } from '@screens/ShoppingCart/styles';
+import { MaterialIcons } from '@expo/vector-icons';
+import {
+  ClearCartButton,
+  ClearCartButtonText,
+  Container,
+  Footer,
+} from '@screens/ShoppingCart/styles';
+import { clearCart, selectTotalCost } from '@store/slices/cart.slice';
+import { RootState } from '@store/store';
+import { CartItem } from '@typings/cart';
+import { FlatList, Text, View } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
+import theme from 'src/theme';
 
 export function ShoppingCart() {
+  const dispatch = useDispatch();
+  const cartItems = useSelector((state: RootState) => state.cart.items);
+  const totalCost = useSelector(selectTotalCost);
+
+  const handleClearCart = () => {
+    dispatch(clearCart());
+  };
+
+  const renderItem = ({ item }: { item: CartItem }) => (
+    <View>
+      <Text>{item.title}</Text>
+      <Text>Quantidade: {item.quantity}</Text>
+    </View>
+  );
+
   return (
     <Container>
-      <Header showBackButton />
-      <Title>Shopping Cart</Title>
+      <Header showBackButton>
+        <Header.Title>carrinho</Header.Title>
+      </Header>
+      <ClearCartButton onPress={handleClearCart}>
+        <MaterialIcons name="delete" size={theme.FONT_SIZE.LG} color={theme.COLORS.RED} />
+        <ClearCartButtonText>Remover todos</ClearCartButtonText>
+      </ClearCartButton>
+      <FlatList data={cartItems} keyExtractor={(item) => item.dealID} renderItem={renderItem} />
+      <Footer>
+        <Text>Total: R$ {totalCost}</Text>
+      </Footer>
     </Container>
   );
 }
--- src/screens/ShoppingCart/styles.ts
+++ src/screens/ShoppingCart/styles.ts
@@ -3,10 +3,28 @@ import styled from 'styled-components/native';
 
 export const Container = styled(SafeAreaView)`
   flex: 1;
-  background-color: ${({ theme }) => theme.COLORS.GRAY_600};
+  background-color: ${(props) => props.theme.COLORS.PURPLE_500};
 `;
 
-export const Title = styled.Text`
-  font-size: ${({ theme }) => theme.FONT_SIZE.XL}px;
-  color: ${({ theme }) => theme.COLORS.WHITE};
+export const ClearCartButton = styled.TouchableOpacity`
+  border-radius: 8px;
+  padding: 4px 8px;
+  margin-left: auto;
+  margin-right: 16px;
+  background-color: ${({ theme }) => theme.COLORS.WHITE};
+  flex-direction: row;
+  gap: 8px;
+`;
+
+export const ClearCartButtonText = styled.Text`
+  font-size: ${({ theme }) => theme.FONT_SIZE.SM}px;
+  font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD};
+  color: ${({ theme }) => theme.COLORS.RED};
+`;
+
+export const Footer = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 16px;
+  background-color: ${({ theme }) => theme.COLORS.WHITE};
 `;
