d449b32a61354fdabc5e925486657548dfbc2882
Antonio Eduardo Ribeiro de Oliveira <antonioeduardo1998@gmail.com>
2024-08-17 14:31:20 -0300

    feat(cart): create slice to handle with shopping cart state

---
+++ src/store/slices/cart.slice.ts
@@ -0,0 +1,45 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { CartState } from '@typings/cart';
+import { Game } from '@typings/games';
+
+const initialState: CartState = {
+  items: [],
+  hasItems: false,
+};
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addItemToCart(state, action: PayloadAction<Game>) {
+      const item = state.items.find((i) => i.dealID === action.payload.dealID);
+      if (item) {
+        item.quantity += 1;
+      } else {
+        state.items.push({ ...action.payload, quantity: 1 });
+      }
+      state.hasItems = true;
+    },
+    removeItemFromCart(state, action: PayloadAction<string>) {
+      state.items = state.items.filter((item) => item.dealID !== action.payload);
+      state.hasItems = state.items.length > 0;
+    },
+    clearCart(state) {
+      state.items = [];
+      state.hasItems = false;
+    },
+  },
+});
+
+export const { addItemToCart, removeItemFromCart, clearCart } = cartSlice.actions;
+
+export const selectCartHasItems = (state: { cart: CartState }) => state.cart.hasItems;
+
+export const selectIsInCart = (dealID: string) => (state: { cart: CartState }) =>
+  state.cart.items.some((item) => item.dealID === dealID);
+
+export const selectTotalCost = (state: { cart: CartState }) => {
+  return state.cart.items.reduce((acc, item) => acc + Number(item.salePrice) * item.quantity, 0);
+};
+
+export default cartSlice.reducer;
