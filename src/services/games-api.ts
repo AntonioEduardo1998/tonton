207dfa011ccdd447fa3ef3131c2fab3ec2e8b209
Antonio Eduardo Ribeiro de Oliveira <antonioeduardo1998@gmail.com>
2024-08-17 18:32:12 -0300

    fix(api): remove transformResponse

--- src/services/games-api.ts
+++ src/services/games-api.ts
@@ -1,15 +1,11 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { Game } from '@typings/games';
 
 export const gamesApi = createApi({
   reducerPath: 'gamesApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'https://www.cheapshark.com/api/1.0/' }),
   endpoints: (builder) => ({
     getGames: builder.query({
-      query: (page = 0) => `deals?pageSize=1&pageNumber=${page}`,
-      transformResponse: (response: Game[]) => {
-        return response;
-      },
+      query: (page = 0) => `deals?pageSize=10&pageNumber=${page}`,
       keepUnusedDataFor: 60,
     }),
   }),
