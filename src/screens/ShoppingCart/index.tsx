14bb3c36e5886560554e02a77860e13b9bea6b3f
Antonio Eduardo Ribeiro de Oliveira <antonioeduardo1998@gmail.com>
2024-08-18 11:35:36 -0300

    fix(shopping-cart): remove use of card component

--- src/screens/ShoppingCart/index.tsx
+++ src/screens/ShoppingCart/index.tsx
@@ -26,11 +26,7 @@ export function ShoppingCart() {
       <FlatList
         data={cartItems}
         keyExtractor={(item) => item.dealID}
-        renderItem={({ item }) => (
-          <Card>
-            <CartItemComponent game={item} />
-          </Card>
-        )}
+        renderItem={({ item }) => <CartItemComponent game={item} />}
       />
       <Footer>
         <Text color={theme.COLORS.PURPLE_900}>Total: R$ {totalCost.toFixed(2)}</Text>
