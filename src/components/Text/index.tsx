ec2c0d7fc58dda687d8b0606a6880088060466cb
Antonio Eduardo Ribeiro de Oliveira <antonioeduardo1998@gmail.com>
2024-08-18 11:34:09 -0300

    fix(text-component): adjust text props

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -1,12 +1,13 @@
 import { StyledText } from '@components/Text/styles';
+import { TextProps as ReactTextProps } from 'react-native';
 
-type TextProps = {
+interface TextProps extends ReactTextProps {
   children: React.ReactNode;
   size?: number;
   color?: string;
   fontFamily?: string;
   align?: 'auto' | 'left' | 'right' | 'center' | 'justify';
-};
+}
 
 export function Text({ children, ...props }: TextProps) {
   return <StyledText {...props}>{children}</StyledText>;
